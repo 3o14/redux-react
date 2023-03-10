0514ffc7102e6ad7cf16fd176250a48779f98668
3o14 <dldnjswn4265@gmail.com>
2023-03-10 11:12:09 +0900

    redux store 컴포넌트 연결

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ todos}) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
@@ -16,9 +17,20 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(todos)}</ul>
     </>
   );
 }
 
-export default Home;
+
+
+// connect : store와 연결
+
+// function 이름은 반드시 mapStateToProps 여야 함
+// mapStateToProps : redux의 state로부터 home(컴포넌트)에 prop으로 data를 전달한다
+function mapStateToProps(state) {
+    return { todos: state };
+}
+
+export default connect(mapStateToProps)(Home);
+
